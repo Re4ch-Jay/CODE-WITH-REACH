b4b7c507d4985ce4d10e11f9d919a9b0b320bea5
Phat Panhareach <panhareach.phat@student.cadt.edu.kh>
2023-02-17 22:28:49 +0700

    makes: mobile dev projects

--- src/Components/Projects Page/Projects.js
+++ src/Components/Projects Page/Projects.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ProjectHero from './ProjectHero';
 import Language from '../Language Icons/Language';
 import { Container } from '@mui/system';
-import { projectItems, projectItems2, projectItems3, projectItems4, projectItems5 } from './Items';
+import { mobileProject2, mobileProject1, projectItems, projectItems2, projectItems3, projectItems4, projectItems5 } from './Items';
 import FrontendProject from './FrontendProject';
 import BackendProject from './BackendProject';
 import { Stack } from '@mui/material';
@@ -16,6 +16,29 @@ function Projects() {
             <p className='text-muted'>Projects & Web I've Created</p>
         </div>
        <Container>
+
+      
+       <div className="text-center text-warning py-5">
+                <h4>Mobile Development Projects</h4>
+        </div>
+        <Stack direction={direction} spacing={spacing}
+          >
+            {mobileProject1.map(item => (
+                <div className='m-2 project-card' key={item.id}>
+                  <BackendProject item={item} yellow={yellow}/>
+                </div>
+            ))}
+        </Stack>
+
+        <Stack direction={direction} spacing={spacing}
+          >
+            {mobileProject2.map(item => (
+                <div className='m-2 project-card' key={item.id}>
+                  <BackendProject item={item} yellow={yellow}/>
+                </div>
+            ))}
+        </Stack>
+
        <div className="text-center text-warning py-5">
                 <h4>Frontend Projects</h4>
         </div>
@@ -69,6 +92,8 @@ function Projects() {
                 </div>
             ))}
         </Stack>
+
+
         <div className="text-center text-warning py-5">
                 <h4>Language & Framework</h4>
                 <p className='text-muted'>That included in those projects</p>
